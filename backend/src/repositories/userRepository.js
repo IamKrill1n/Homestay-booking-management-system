418f2f4c216624b9fc3b7ca2b1f235032386a0e8
Tatsnien <nguyenbaochau882005@gmai.com>
2026-06-13 22:28:39 +0700

    update CRUD to userRepository.js

--- backend/src/repositories/userRepository.js
+++ backend/src/repositories/userRepository.js
@@ -18,7 +18,7 @@ function mapRowToUser (row) {
 export async function findUserByEmail(email) {
   try {
     const { rows } = await pool.query(
-      `SELECT * FROM users WHERE email = $1`,
+      `SELECT * FROM users WHERE email = $1;`,
       [email]
     );
 
@@ -26,24 +26,69 @@ export async function findUserByEmail(email) {
 
     return mapRowToUser(rows[0]);
 
-  } catch (error) {
-    console.error("Database error in findUserByEmail:", error);
-    throw error;
+  } catch (err) {
+    console.error("Database error in findUserByEmail:", err);
+    throw err;
   }
 }
 
 export async function findUserByID(userID) {
   try {
     const { rows } = await pool.query(
-      `SELECT * FROM users WHERE user_id = $1`,
+      `SELECT * FROM users WHERE user_id = $1;`,
       [userID]
     );
   
     if (rows.length == 0) return null;
   
-      return mapRowToUser(rows[0]);
-  } catch (error) {
-    console.error("Database error in findUserByID:", error);
-    throw error;
+    return mapRowToUser(rows[0]);
+  } catch (err) {
+    console.error("Database error in findUserByID:", err);
+    throw err;
+  }
+}
+
+export async function createUser(user) {
+  try {
+    await pool.query(
+      `INSERT INTO users(password, first_name, last_name, email, phone_number, role)
+      VALUES ($1, $2, $3, $4, $5, $6);`,
+      [user.password, user.firstName, user.lastName, user.email, user.phoneNumber, user.role]
+    );
+  } catch (err) {
+    console.error("Database error in addUser:", err);
+    throw err;
+  }
+}
+
+export async function updateUser(user) {
+  try {
+    await pool.query(
+      `UPDATE users 
+      SET 
+        password = $2, 
+        first_name = $3, 
+        last_name = $4, 
+        email = $5, 
+        phone_number = $6, 
+        role = $7
+      WHERE user_id = $1;`,
+      [user.userID, user.password, user.firstName, user.lastName, user.email, user.phoneNumber, user.role]
+    );
+  } catch (err) {
+    console.error("Database error in addUser:", err);
+    throw err;
+  }
+}
+
+export async function deleteUserByID(userID) {
+  try {
+    await pool.query(
+      `DELETE FROM users WHERE user_id = $1;`,
+      [userID]
+    )
+  } catch (err) {
+    console.error("Database error in deleteUserByID:", err);
+    throw err;
   }
 }
